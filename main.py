a9920a204c601ee2b65f5a0b3c5abbff1da92d14
Konrad Krenzlin <git@krenzlin.com>
2018-10-24 12:15:45 +0200

    use click's file type

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import click
 
 
 @click.command()
-@click.argument('filename')
+@click.argument('inputfile', type=click.File())
 @click.argument('savedir')
-def main(filename, savedir):
-    print('reading: {}'.format(filename))
-
-    with open(filename) as f:
-        for line in f:
-            url = line.strip()
-            if url:
-                print(url)
+def main(inputfile, savedir):
+    for line in inputfile:
+        url = line.strip()
+        if url:
+            print(url)
 
 
 if __name__ == "__main__":
