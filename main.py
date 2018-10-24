184c2054576b0425c3302e28eec4167a8c8af1cc
Konrad Krenzlin <konrad@zentralblatt-math.org>
2018-10-24 14:17:55 +0200

    refactor into fetch_url and save_to_file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlparse
+from pathlib import Path
 
 import click
 import requests
@@ -9,10 +10,29 @@ def url_to_path(url):
     return '{}_{}{}'.format(parsed.scheme, parsed.netloc, parsed.path)
 
 
-def download(url, filename):
+def fetch_url(url):
     r = requests.get(url)
-    with open(filename, 'wb') as f:
-        f.write(r.content)
+    return r.content
+
+
+def save_to_file(data, file_path):
+    path = Path(file_path)
+
+    # mkdir -p (if not exists)
+    try:
+        path.parent.mkdir(parents=True)
+    except FileExistsError:
+        pass
+
+    # write to file
+    with path.open('wb') as f:
+        f.write(data)
+
+
+def download(url, file_path):
+    data = fetch_url(url)
+    if data:
+        save_to_file(data=data, file_path=file_path)
 
 
 @click.command()
@@ -22,7 +42,9 @@ def main(inputfile, savedir):
     for line in inputfile:
         url = line.strip()
         if url:
-            print(url)
+            path = Path(savedir) / url_to_path(url)
+            print(url, path)
+            download(url, str(path))
 
 
 if __name__ == "__main__":
