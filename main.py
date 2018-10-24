0566f753d80780595fe5a6d1d0f612a178953cb4
Konrad Krenzlin <git@krenzlin.com>
2018-10-24 12:11:00 +0200

    use click for cli

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sys
+import click
 
 
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    savedir = sys.argv[2]
+@click.command()
+@click.argument('filename')
+@click.argument('savedir')
+def main(filename, savedir):
     print('reading: {}'.format(filename))
 
     with open(filename) as f:
@@ -11,3 +12,7 @@ if __name__ == "__main__":
             url = line.strip()
             if url:
                 print(url)
+
+
+if __name__ == "__main__":
+    main()
